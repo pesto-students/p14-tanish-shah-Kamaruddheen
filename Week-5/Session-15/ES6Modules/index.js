246ca8f75df2da017b786199e87cddefca6e8e0f
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2023-11-08 03:08:11 +0530

    JS: ES6 Modules

---
+++ Week-5/Session-15/ES6Modules/index.js
@@ -0,0 +1,8 @@
+// Importing individual items
+import { add, subtract } from './calculator.js'
+
+let a = 10
+let b = 5
+
+console.log("Add : ", add(a, b))
+console.log("Subtract : ", subtract(a, b))
