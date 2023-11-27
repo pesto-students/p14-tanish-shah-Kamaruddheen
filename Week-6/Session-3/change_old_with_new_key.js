033283b7a3879b6a1b86cbe8b48b4e3771644575
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2023-11-27 21:05:04 +0530

    Array: Replace old value with new value
    + practice traverse program

---
+++ Week-6/Session-3/change_old_with_new_key.js
@@ -0,0 +1,15 @@
+function replaceKey(arr, oldKey, newKey) {
+  // PLACEHOLDER_JAVASCRIPT_REPLACE_KEY_BODY
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === oldKey) {
+      arr[i] = newKey;
+    }
+  }
+
+  return arr;
+}
+
+// console.log(replaceKey([1, 2, 3, 2, 4, 2, 5], 2, 6)); // [1,6,3,6,4,6,5]
+// console.log(replaceKey([10, 20, 30, 40, 50], 30, 35)); // [10,20,35,40,50]
+// console.log(replaceKey([5, 5, 5, 5], 5, 2)); // [2,2,2,2]
+// console.log(replaceKey([1, 2, 3, 4, 5], 6, 8)); // [1,2,3,4,5]
