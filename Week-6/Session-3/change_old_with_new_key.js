a23b1f10bb20d2a91c27a2ed9f52c71bcf53c0e6
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2023-11-28 19:03:51 +0530

    Array: Delete elements in an array

--- Week-6/Session-3/change_old_with_new_key.js
+++ Week-6/Session-3/change_old_with_new_key.js
@@ -1,3 +1,19 @@
+/*
+
+Input:
+
+1,2,3,2,4,2,5
+2
+6
+
+Output:
+[1, 6, 3, 6, 4, 6, 5]
+
+Explanation:
+There are total 3 2s presenting a array which is replaced by 6
+
+*/
+
 function replaceKey(arr, oldKey, newKey) {
   // PLACEHOLDER_JAVASCRIPT_REPLACE_KEY_BODY
   for (let i = 0; i < arr.length; i++) {
@@ -9,7 +25,7 @@ function replaceKey(arr, oldKey, newKey) {
   return arr;
 }
 
-// console.log(replaceKey([1, 2, 3, 2, 4, 2, 5], 2, 6)); // [1,6,3,6,4,6,5]
-// console.log(replaceKey([10, 20, 30, 40, 50], 30, 35)); // [10,20,35,40,50]
-// console.log(replaceKey([5, 5, 5, 5], 5, 2)); // [2,2,2,2]
-// console.log(replaceKey([1, 2, 3, 4, 5], 6, 8)); // [1,2,3,4,5]
+console.log(replaceKey([1, 2, 3, 2, 4, 2, 5], 2, 6)); // [1,6,3,6,4,6,5]
+console.log(replaceKey([10, 20, 30, 40, 50], 30, 35)); // [10,20,35,40,50]
+console.log(replaceKey([5, 5, 5, 5], 5, 2)); // [2,2,2,2]
+console.log(replaceKey([1, 2, 3, 4, 5], 6, 8)); // [1,2,3,4,5]
--- Week-6/Week-6.md
+++ Week-6/Week-6.md
@@ -1,4 +1,4 @@
-### Session 1: Intro To DSA
+### Session 1: Introduction to Data Structures and Algorithms
 
 - Arithimatic Operations Assignment
   - [Product of Hcf and Lcm](Session-1/Arithimatic%20Operations/lcm_hcf.js)
@@ -9,10 +9,15 @@
   - [Reverse a Number](Session-1/Loops/reverse_num.js)
   - [Is Pallindrome ?](Session-1/Loops/palindrome.js)
 
-### Session 2: Arrays
+### Session 2: Exploring the Fundamentals of Arrays
 
   - [Count Elements](Session-2/count_array.js)
   - [Find non duplicate element in an array.](Session-2/unique_element.js)
   - [Intersection of Two Arrays](Session-2/intersect_array.js)
   - [Get the sum of the maximum and minimum values in an array](Session-2/sum_max_min.js)
   - [Pairs of sum](Session-2/sum_pair_arrays.js)
+
+### Session 3: Problem Solving on Arrays
+
+  - [Old Key New Key](Session-3/change_old_with_new_key.js)
+  - [Delete elements in an array](Session-3/del_element_array.js)
